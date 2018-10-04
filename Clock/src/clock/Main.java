356de12f7047b965aed15e0489e6b21c0fc120a2
Ali Mac <mralimac@users.noreply.github.com>
2018-10-04 16:45:12 +0100

    Adding better Comments

--- Clock/src/clock/Main.java
+++ Clock/src/clock/Main.java
@@ -6,11 +6,17 @@ public class Main
 {	
 	public static void main(String[] args) 
 	{	
+		//userInterface is an object that is used to mostly gather user input in regards to the clock time		
 		UserInput userInterface = new UserInput();
+				
 		if(userInterface.useUserInput()){
+			
+			//This generates a new clock with parameters
 			ClockDisplay clock = new ClockDisplay(userInterface.getHour(),userInterface.getMin(),userInterface.getSec(), userInterface.use12Hr());
 			while(true)
 			{
+				//Once the clock has been created, these two lines will display the time in console
+				//and then tick the clock on to the next second
 				System.out.println(clock.displayClock());
 				clock.timeTick();
 			}
